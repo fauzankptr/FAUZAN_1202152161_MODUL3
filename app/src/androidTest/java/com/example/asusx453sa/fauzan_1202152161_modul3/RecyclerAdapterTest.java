22d12e44642bba9cb8a1e8d37ac1de90026a4c14
fauzankptr <ekaputra2501@gmail.com>
2018-02-25 20:39:31 +0700

    FAUZAN_1202152161_MODUL3

---
+++ app/src/androidTest/java/com/example/asusx453sa/fauzan_1202152161_modul3/RecyclerAdapterTest.java
@@ -0,0 +1,21 @@
+package com.example.asusx453sa.fauzan_1202152161_modul3;
+
+import org.junit.Test;
+
+/**
+ * Created by ASUS X453SA on 2/23/2018.
+ */
+
+public class RecyclerAdapterTest {
+    @Test
+    public void onCreateViewHolder() throws Exception {
+    }
+
+    @Test
+    public void onBindViewHolder() throws Exception {
+    }
+
+    @Test
+    public void getItemCount() throws Exception {
+    }
+}
